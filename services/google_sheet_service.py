aea433c193c2edd9592d3660bc4d7b35a0e220d3
crplautomation <info@chetanroadlinespvtltd.com>
2026-06-29 11:28:50 +0530

    Fix missing os import

--- services/google_sheet_service.py
+++ services/google_sheet_service.py
@@ -1,29 +1,95 @@
-import json
-import tempfile
 import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-if os.path.exists(SERVICE_ACCOUNT):
+from config import SPREADSHEET_ID, MASTER_SHEET, SERVICE_ACCOUNT
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        SERVICE_ACCOUNT,
-        SCOPES
-    )
+# =====================================================
+# GOOGLE SHEETS CONFIG
+# =====================================================
 
-else:
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
 
-    service_json = os.environ["GOOGLE_SERVICE_ACCOUNT"]
+_client = None
+_spreadsheet = None
 
-    with tempfile.NamedTemporaryFile(
-        mode="w",
-        suffix=".json",
-        delete=False
-    ) as f:
 
-        f.write(service_json)
+# =====================================================
+# CONNECT
+# =====================================================
 
-        temp_file = f.name
+def get_client():
+    global _client
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        temp_file,
-        SCOPES
-    )
+    if _client is None:
+
+        # Render Secret File
+        if os.path.exists("/etc/secrets/service_account.json"):
+            json_file = "/etc/secrets/service_account.json"
+
+        # Local PC
+        else:
+            json_file = SERVICE_ACCOUNT
+
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            json_file,
+            SCOPES
+        )
+
+        _client = gspread.authorize(creds)
+
+    return _client
+
+# =====================================================
+# SPREADSHEET
+# =====================================================
+
+def get_spreadsheet():
+    global _spreadsheet
+
+    if _spreadsheet is None:
+
+        _spreadsheet = get_client().open_by_key(
+            SPREADSHEET_ID
+        )
+
+    return _spreadsheet
+
+
+# =====================================================
+# WORKSHEETS
+# =====================================================
+
+def get_master():
+    return get_spreadsheet().worksheet(MASTER_SHEET)
+
+
+def get_tracking():
+    return get_spreadsheet().worksheet("Tracking")
+
+
+def get_statement():
+    return get_spreadsheet().worksheet("Statement")
+
+
+def get_trip():
+    return get_spreadsheet().worksheet("Trip")
+
+
+def get_goods():
+    return get_spreadsheet().worksheet("Goods")
+
+
+def get_wallet():
+    return get_spreadsheet().worksheet("Wallet")
+
+
+def get_manual():
+    return get_spreadsheet().worksheet("MANUAL")
+
+
+def get_bank_statement():
+    return get_spreadsheet().worksheet("Bank Statement")
